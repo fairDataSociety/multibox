f62f497810f4d903fba2f1fd9bac548a68ba58f6
significance <dan@1up.digital>
2019-05-09 09:47:39 +0100

    adds first passing test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const Multibox = artifacts.require("Multibox");
 const KeyValueTree = artifacts.require("KeyValueTree");
+const OOO = artifacts.require("OOO");
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(Multibox);
+  await deployer.deploy(OOO);
   let mb = await Multibox.deployed();
 };
--- test/multibox.js
+++ test/multibox.js
@@ -1,35 +1,17 @@
-const MultiBox = artifacts.require("Multibox");
+const Multibox = artifacts.require("Multibox");
 const KeyValueTree = artifacts.require("KeyValueTree");
 
 
 contract('MultiBox', (accounts) => {
   it('should initialise and create root folder', async () => {
-    const multiBoxInstance = await MultiBox.deployed();
-    const keyValueTreeInstance = await KeyValueTree.deployed();
+    const multiBoxInstance = await Multibox.deployed();
 
-    console.log(1, multiBoxInstance.address, keyValueTreeInstance.address);
+    const multiBoxInstance2 = await Multibox.at(multiBoxInstance.address);
 
-    const init = await multiBoxInstance.init.call({ from: accounts[0] });
+    const keyValueTreeContractAddress = await multiBoxInstance.init({ from: accounts[0] });
 
-    const roots = await multiBoxInstance.getRoots.call({ from: accounts[0] });
+    const roots = await multiBoxInstance.getRoots({ from: accounts[0] });
 
-    debugger
-
-    // should have deployed a new contract KeyValueTree
-    assert.equal(roots.length, 1, "root was not created...");
-
-    // const kvtContract = KeyValueTree.contractFromAddress(kvtcAddresses[0]);
-    // const keyValueTreeContract = await multiBoxInstance.getRoot(roots[0]).call({ from: accounts[0] });
-    // // assert.equal(roots.length, 1, "root was not created...");    
+    assert.equal(roots.length, 1, "root was not created...");   
   });
-
-  it('should create new folder', async () => {
-    // const multiBoxInstance = await MultiBox.deployed();
-
-    // const init = await multiBoxInstance.init.call({ from: accounts[0] });
-
-    // const roots = await multiBoxInstance.getRoots.call({ from: accounts[0] });
-
-    // assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-  });  
 });
